136b8e3ec32a0dd3c9fe15b3a446f0d967208b47
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-09-19 16:26:14 -0700

    created variables to store arrays of the various characters and a general pool of selected characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,24 @@
-// Assignment Code
+///////////////////////////////////////////////////////////////////////
+// PASSWORD GENERATOR
+//
+// * For this assignment, you will not be changing the HTML and CSS at all.
+//
+// * You will need a generatePassword function is called when the user
+//   clicks the Generate Password button.
+//
+// * You can create other functions that are called from within
+//   generatePassword
+//
+// * Gather user input with prompt's and confirm's
+
+///////////////////////////////////////////////////////////////////////
+// DO NOT TOUCH THIS CODE
+//
+// This code handles:
+// * clicking the Generate Password
+// * writing the password to the screen
+//
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,55 +27,69 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
   var newPassword;
 
   // Prompt asking length of the password 8-128 characters and store selected response
-  var passLength = prompt("How long would you like your password to be? Choose between 8-128.");
-
-  console.log(passLength);
+  var length = prompt("How long would you like your password to be? Choose between 8-128.");
+  console.log(length);
 
   // Confirm message to choose lowercase and store selected response
-  var passLower = confirm("Would you like lowercase letters?");
-
-  console.log(passLower);
+  var lowercase = confirm("Would you like lowercase letters?");
+  console.log(lowercase);
 
   // Confirm message to choose uppercase and store selected response
-  var passUpper = confirm("Would you like uppercase letters?");
-
-  console.log(passUpper);
+  var uppercase = confirm("Would you like uppercase letters?");
+  console.log(uppercase);
 
   // Confirm message to choose numeric and store selected response
-  var passNum = confirm("Would you like numbers?");
-
-  console.log(passNum);
+  var numbers = confirm("Would you like numbers?");
+  console.log(numbers);
 
   // Confirm message to choose special characters and store selected response
-  var passSpecial = confirm("Would you like special characters?");
-
-  console.log(passSpecial);
+  var special = confirm("Would you like special characters?");
+  console.log(special);
 
   // choice array made up of those if true; iterate
-  var choice = [];
+  var letterPool = [];
 
-  // 26 letters lowercase
+  // Did the user choose lowercase letters? (26)
   var lowLetters = "abcdefghijklmnopqrstuvwxyz";
   var lowLettersArr = lowLetters.split("");
 
-  if (passLower === true) {
-    choice.push(...lowLettersArr);
+  if (lowercase === true) {
+    letterPool.push(lowLettersArr);
+    console.log(letterPool);
   }
 
-  console.log(choice);
+  // Did the user choose uppercase letters? (26)
+  var uppLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var uppLettersArr = uppLetters.split("");
 
-  // 26 letters Uppercase
+  if (uppercase === true) {
+    letterPool.push(uppLettersArr);
+    console.log(letterPool);
+  }
 
-  // 10 numbers
+  // Did the user choose numbers? (10)
+  var numChar = "0123456789";
+  var numCharArr = numChar.split("");
 
-  // specials
+  if (numbers === true) {
+    letterPool.push(numCharArr);
+    console.log(letterPool);
+  }
+
+  // Did the user choose special characters? (28)
+  var specialChar = "~`!@#$%^&*()-_=+[]{}/?:;<>,.";
+  var specialCharArr = specialChar.split("");
+
+  if (special === true) {
+    letterPool.push(specialCharArr);
+    console.log(letterPool);
+  }
 
   // Math.random(); default, would need to know the length selected, then choose randomly from choice array
 
@@ -64,3 +98,5 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////////////////////
